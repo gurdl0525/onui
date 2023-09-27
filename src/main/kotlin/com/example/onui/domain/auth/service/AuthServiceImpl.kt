1239f7e9f50c683c83cc7d1e721a1e178e8d39cf
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-27 18:02:16 +0900

    ♻️ :: 코드 다듬기

--- src/main/kotlin/com/example/onui/domain/auth/service/AuthServiceImpl.kt
+++ src/main/kotlin/com/example/onui/domain/auth/service/AuthServiceImpl.kt
@@ -1,26 +1,14 @@
 package com.example.onui.domain.auth.service
 
-import com.example.onui.domain.auth.presentation.dto.response.TokenResponse
+import com.example.onui.global.common.facade.UserFacade
 import com.example.onui.global.config.jwt.TokenProvider
-import com.example.onui.global.config.jwt.env.TokenProperty
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
 
 @Service
 class AuthServiceImpl(
     private val tokenProvider: TokenProvider,
-    private val tokenProperty: TokenProperty
+    private val userFacade: UserFacade
 ): AuthService {
 
-    override fun reissue(refreshToken: String): TokenResponse {
-
-        val (access, refresh) = tokenProvider.reissue(refreshToken)
-
-        return TokenResponse (
-            access,
-            LocalDateTime.now().plusSeconds(tokenProperty.accessExp),
-            refresh,
-            LocalDateTime.now().plusSeconds(tokenProperty.refreshExp)
-        )
-    }
+    override fun reissue(refreshToken: String) = tokenProvider.reissue(refreshToken)
 }
