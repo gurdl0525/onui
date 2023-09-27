390c059c840bcbf51a8f703468ad824597d4bd21
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-27 18:12:59 +0900

    ⭐️ :: property 추가

--- src/main/kotlin/com/example/onui/infra/feign/google/dto/GoogleInfoResponse.kt
+++ src/main/kotlin/com/example/onui/infra/feign/google/dto/GoogleInfoResponse.kt
@@ -1,6 +1,12 @@
 package com.example.onui.infra.feign.google.dto
 
 data class GoogleInfoResponse(
+    val sub: String,
+    val name: String,
+    val givenName: String,
+    val familyName: String?,
+    val picture: String,
     val email: String,
-    val username: String
+    val emailVerified: Boolean,
+    val locale: String
 )
