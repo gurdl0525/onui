37c171c6ffdaf829d7366c54286495c8351e2872
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-04 14:31:55 +0900

    ⭐️ :: DiaryRepository

---
+++ src/main/kotlin/com/example/onui/domain/diary/repository/DiaryRepository.kt
@@ -0,0 +1,15 @@
+﻿package com.example.onui.domain.diary.repository
+
+import com.example.onui.domain.diary.entity.Diary
+import com.example.onui.domain.user.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface DiaryRepository: JpaRepository<Diary, UUID?> {
+
+    fun findAllByUserAndYearAndMonth(user: User, year: Int, month: Int): MutableList<Diary>?
+
+    fun existsByUserAndYearAndMonth(user: User, year: Int, month: Int): Boolean
+}
