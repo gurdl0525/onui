24faa63e1d6847fb7ca23c38c00186f98eeaadb3
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-04 14:33:07 +0900

    ⭐️ :: ALREADY_WROTE_DIARY, DIARY_NOT_FOUND

---
+++ src/main/kotlin/com/example/onui/domain/diary/exception/AlreadyWroteDiaryException.kt
@@ -0,0 +1,6 @@
+﻿package com.example.onui.domain.diary.exception
+
+import com.example.onui.global.config.error.data.ErrorCode
+import com.example.onui.global.config.error.exception.BusinessException
+
+object AlreadyWroteDiaryException : BusinessException(ErrorCode.ALREADY_WROTE_DIARY)
