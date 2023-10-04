ceb7b967f5ede79ac4a7f43e303edaf1548f61b1
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-04 14:37:34 +0900

    ⭐️ :: CreateDiaryRequest

---
+++ src/main/kotlin/com/example/onui/domain/diary/presentation/request/CreateDiaryRequest.kt
@@ -0,0 +1,15 @@
+﻿package com.example.onui.domain.diary.presentation.request
+
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+
+data class CreateDiaryRequest(
+
+    val text: String?,
+
+    @field:NotNull(message = "null일 수 없습니다.")
+    @field:Min(1, message = "1보다 작을 수 없습니다.")
+    @field:Max(5, message = "5보다 클 수 없습니다.")
+    val mood: Int?,
+)
