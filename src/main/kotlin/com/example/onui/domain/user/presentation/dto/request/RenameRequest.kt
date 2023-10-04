fe45705811b20d73a948b810a863389fef81dcd9
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-04 14:38:56 +0900

    ⭐️ :: RenameRequest

---
+++ src/main/kotlin/com/example/onui/domain/user/presentation/dto/request/RenameRequest.kt
@@ -0,0 +1,9 @@
+﻿package com.example.onui.domain.user.presentation.dto.request
+
+import javax.validation.constraints.NotBlank
+
+data class RenameRequest(
+
+    @field:NotBlank(message = "null일 수 없습니다.")
+    val name: String?
+)
