e52054fd25c9e4d586b8bef13cdcecbaf66f2e12
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-04 14:37:59 +0900

    ⭐️ :: DiaryDetailResponse

---
+++ src/main/kotlin/com/example/onui/domain/diary/presentation/response/DiaryDetailResponse.kt
@@ -0,0 +1,11 @@
+﻿package com.example.onui.domain.diary.presentation.response
+
+import java.time.LocalDate
+import java.util.UUID
+
+data class DiaryDetailResponse(
+    val id: UUID,
+    val text: String?,
+    val mood: Int,
+    val createdAt: LocalDate
+)
