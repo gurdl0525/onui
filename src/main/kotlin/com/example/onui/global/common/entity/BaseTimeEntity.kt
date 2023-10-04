ae347020cb50929566190d6e8135392633b0de07
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-04 14:34:16 +0900

    ⭐️ :: BaseTimeEntity

---
+++ src/main/kotlin/com/example/onui/global/common/entity/BaseTimeEntity.kt
@@ -0,0 +1,30 @@
+﻿package com.example.onui.global.common.entity
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDate
+import javax.persistence.Column
+import javax.persistence.EntityListeners
+import javax.persistence.MappedSuperclass
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseTimeEntity(
+    createdAt: LocalDate
+) {
+
+    @Column(name = "year", nullable = false)
+    var year: Int = createdAt.year
+        protected set
+
+    @Min(1) @Max(12)
+    @Column(name = "month", nullable = false)
+    var month: Int = createdAt.monthValue
+        protected set
+
+    @Min(1) @Max(31)
+    @Column(name = "day", nullable = false)
+    var day: Int = createdAt.dayOfMonth
+        protected set
+}
