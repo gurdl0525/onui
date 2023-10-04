254457ea51bc8093c51fca4a4aa12f49fb706b63
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-04 14:32:14 +0900

    ⭐️ :: DiaryService

---
+++ src/main/kotlin/com/example/onui/domain/diary/service/DiaryService.kt
@@ -0,0 +1,15 @@
+﻿package com.example.onui.domain.diary.service
+
+import com.example.onui.domain.diary.presentation.request.CreateDiaryRequest
+import com.example.onui.domain.diary.presentation.response.DiaryDetailResponse
+import com.example.onui.domain.diary.presentation.response.DiaryListResponse
+import java.util.*
+
+interface DiaryService {
+
+    fun createDiary(req: CreateDiaryRequest): DiaryDetailResponse
+
+    fun getDiaryByMonth(year: Int, month: Int): DiaryListResponse
+
+    fun getDetailById(id: UUID): DiaryDetailResponse
+}
