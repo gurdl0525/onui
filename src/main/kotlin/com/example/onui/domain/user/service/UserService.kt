369c0dac8c44f225ab5c8dc44aa7a3506141c07b
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-04 14:33:36 +0900

    ⭐️ :: UserService

---
+++ src/main/kotlin/com/example/onui/domain/user/service/UserService.kt
@@ -0,0 +1,8 @@
+﻿package com.example.onui.domain.user.service
+
+import com.example.onui.domain.user.presentation.dto.response.UserProfileResponse
+
+interface UserService {
+
+    fun rename(name: String): UserProfileResponse
+}
