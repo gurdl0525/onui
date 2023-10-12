8bae644b7791805521255d80b7e470a7bc024bda
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-12 16:40:59 +0900

    ⭐️ :: UpdateDiaryRequest

---
+++ src/main/kotlin/com/example/onui/domain/diary/presentation/request/UpdateDiaryRequest.kt
@@ -0,0 +1,29 @@
+﻿package com.example.onui.domain.diary.presentation.request
+
+import java.util.UUID
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+
+data class UpdateDiaryRequest(
+
+    @field:NotNull
+    val id: UUID?,
+
+    @field:NotBlank
+    val title: String?,
+
+    @field:NotBlank
+    val content: String?,
+
+    @field:NotNull(message = "null일 수 없습니다.")
+    @field:Min(1, message = "1보다 작을 수 없습니다.")
+    @field:Max(5, message = "5보다 클 수 없습니다.")
+    val mood: Int?,
+
+    @field:NotEmpty
+    @field:NotNull
+    val tagList: MutableList<String>?
+)
