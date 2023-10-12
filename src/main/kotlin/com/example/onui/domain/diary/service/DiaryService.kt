bd735c6e696ec5164b277ac959133c3582c57c69
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-12 16:43:26 +0900

    ⭐️ :: update diary

--- src/main/kotlin/com/example/onui/domain/diary/service/DiaryService.kt
+++ src/main/kotlin/com/example/onui/domain/diary/service/DiaryService.kt
@@ -1,6 +1,7 @@
 ﻿package com.example.onui.domain.diary.service
 
 import com.example.onui.domain.diary.presentation.request.CreateDiaryRequest
+import com.example.onui.domain.diary.presentation.request.UpdateDiaryRequest
 import com.example.onui.domain.diary.presentation.response.DiaryDetailResponse
 import com.example.onui.domain.diary.presentation.response.DiaryListResponse
 import java.util.*
@@ -12,4 +13,6 @@ interface DiaryService {
     fun getDiaryByMonth(year: Int, month: Int): DiaryListResponse
 
     fun getDetailById(id: UUID): DiaryDetailResponse
+
+    fun update(req: UpdateDiaryRequest): DiaryDetailResponse
 }
