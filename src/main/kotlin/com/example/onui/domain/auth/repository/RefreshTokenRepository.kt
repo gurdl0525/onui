6682cc67562f8dcc3bfec8724d02f42f6565224b
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-12 16:28:32 +0900

    📝 :: rename

--- src/main/kotlin/com/example/onui/domain/auth/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/example/onui/domain/auth/repository/RefreshTokenRepository.kt
@@ -3,10 +3,9 @@ package com.example.onui.domain.auth.repository
 import com.example.onui.domain.auth.entity.RefreshToken
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import javax.validation.constraints.Email
 
 @Repository
 interface RefreshTokenRepository: CrudRepository<RefreshToken, String> {
 
-    fun findByEmail(email: String): RefreshToken?
+    fun findBySub(sub: String): RefreshToken?
 }
