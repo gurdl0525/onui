ea30e661ddb6119bf7f3ad6f955396131db4aa48
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-12 16:40:46 +0900

    ⭐️ :: TimelineRepository

---
+++ src/main/kotlin/com/example/onui/domain/timeline/repository/TimelineRepository.kt
@@ -0,0 +1,13 @@
+﻿package com.example.onui.domain.timeline.repository
+
+import com.example.onui.domain.timeline.entity.Timeline
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import java.time.DayOfWeek
+import java.util.*
+
+interface TimelineRepository: JpaRepository<Timeline, UUID?> {
+
+    fun findAllByDayOfWeek(dayOfWeek: DayOfWeek, pageable: Pageable): Page<Timeline>
+}
