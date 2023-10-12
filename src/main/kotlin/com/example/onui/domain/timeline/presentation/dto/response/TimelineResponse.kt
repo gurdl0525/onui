6c5709110170b11bae05ba28bf56c0ac012e857d
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-12 16:40:32 +0900

    ⭐️ :: TimelineResponse

---
+++ src/main/kotlin/com/example/onui/domain/timeline/presentation/dto/response/TimelineResponse.kt
@@ -0,0 +1,16 @@
+﻿package com.example.onui.domain.timeline.presentation.dto.response
+
+import java.time.DayOfWeek
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class TimelineResponse (
+    val id: UUID,
+    val title: String,
+    val content: String,
+    val mood: Int,
+    val tag: MutableList<String>,
+    val dayOfWeek: DayOfWeek,
+    val createdAt: LocalDateTime,
+    val isUpdated: Boolean
+)
