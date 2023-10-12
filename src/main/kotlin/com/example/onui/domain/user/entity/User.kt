afa6ed37815705b455481b5a119ba3be33ba3e69
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-12 16:31:07 +0900

    📝 :: rename

--- src/main/kotlin/com/example/onui/domain/user/entity/User.kt
+++ src/main/kotlin/com/example/onui/domain/user/entity/User.kt
@@ -1,7 +1,6 @@
 package com.example.onui.domain.user.entity
 
 import com.example.onui.domain.diary.entity.Diary
-import com.example.onui.domain.user.entity.type.UserType
 import com.example.onui.domain.user.presentation.dto.response.UserProfileResponse
 import org.hibernate.annotations.DynamicUpdate
 import java.util.*
@@ -10,9 +9,8 @@ import javax.persistence.*
 @Entity(name = "user")
 @DynamicUpdate
 class User(
-    email: String,
+    sub: String,
     name: String,
-    type: UserType,
     id: UUID? = null
 ) {
 
@@ -21,23 +19,19 @@ class User(
     var id: UUID? = id
         protected set
 
-    @Column(name = "email", nullable = false)
-    var email: String = email
+    @Column(name = "sub", nullable = false, length = 30, unique = true)
+    var sub: String = sub
         protected set
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, length = 255)
     var name: String = name
         protected set
 
     @OneToMany(mappedBy = "user", cascade = [CascadeType.REMOVE])
     var diaryList: MutableList<Diary> = arrayListOf()
 
-    @Enumerated(EnumType.STRING)
-    var type: UserType = type
-        protected set
-
     fun toResponse() = UserProfileResponse(
-        this.email,
+        this.sub,
         this.name
     )
 }
