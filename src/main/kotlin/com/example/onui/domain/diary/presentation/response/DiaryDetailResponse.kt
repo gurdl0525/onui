c00692aca3f862ee43ca87b725c95b0bf96b6086
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-12 16:43:49 +0900

    ♻️ :: 구조 조정

--- src/main/kotlin/com/example/onui/domain/diary/presentation/response/DiaryDetailResponse.kt
+++ src/main/kotlin/com/example/onui/domain/diary/presentation/response/DiaryDetailResponse.kt
@@ -5,7 +5,9 @@ import java.util.UUID
 
 data class DiaryDetailResponse(
     val id: UUID,
-    val text: String?,
+    val title: String,
+    val content: String,
     val mood: Int,
+    val tag: MutableList<String>,
     val createdAt: LocalDate
 )
