2269091e7d0e22899cfa1e8ed1b83bf9a063c5e6
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-12 16:43:44 +0900

    ♻️ :: 구조 조정

--- src/main/kotlin/com/example/onui/domain/diary/presentation/request/CreateDiaryRequest.kt
+++ src/main/kotlin/com/example/onui/domain/diary/presentation/request/CreateDiaryRequest.kt
@@ -2,14 +2,24 @@
 
 import javax.validation.constraints.Max
 import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 data class CreateDiaryRequest(
 
-    val text: String?,
+    @field:NotBlank
+    val title: String?,
+
+    @field:NotBlank
+    val content: String?,
 
     @field:NotNull(message = "null일 수 없습니다.")
     @field:Min(1, message = "1보다 작을 수 없습니다.")
     @field:Max(5, message = "5보다 클 수 없습니다.")
     val mood: Int?,
+
+    @field:NotEmpty
+    @field:NotNull
+    val tagList: MutableList<String>?
 )
