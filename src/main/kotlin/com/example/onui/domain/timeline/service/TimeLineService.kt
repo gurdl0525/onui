0eff2d7efe1d0c8767633885182a609c9918ba77
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-12 16:39:27 +0900

    ⭐️ :: timeline.post, timeline.searchByDayOfWeek

---
+++ src/main/kotlin/com/example/onui/domain/timeline/service/TimeLineService.kt
@@ -0,0 +1,13 @@
+﻿package com.example.onui.domain.timeline.service
+
+import com.example.onui.domain.timeline.presentation.dto.response.TimelineResponse
+import org.springframework.data.domain.Page
+import java.time.DayOfWeek
+import java.util.*
+
+interface TimeLineService {
+
+    fun post(id: UUID): TimelineResponse
+
+    fun searchByDayOfWeek(idx: Int, size: Int, dayOfWeek: DayOfWeek): Page<TimelineResponse>
+}
