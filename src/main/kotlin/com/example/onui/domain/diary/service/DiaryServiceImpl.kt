9dc8813b42526e559c21528b7e59144874c7c38c
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-12 16:43:22 +0900

    ⭐️ :: update diary

--- src/main/kotlin/com/example/onui/domain/diary/service/DiaryServiceImpl.kt
+++ src/main/kotlin/com/example/onui/domain/diary/service/DiaryServiceImpl.kt
@@ -4,36 +4,42 @@ import com.example.onui.domain.diary.entity.Diary
 import com.example.onui.domain.diary.exception.AlreadyWroteDiaryException
 import com.example.onui.domain.diary.exception.DiaryNotFoundException
 import com.example.onui.domain.diary.presentation.request.CreateDiaryRequest
+import com.example.onui.domain.diary.presentation.request.UpdateDiaryRequest
 import com.example.onui.domain.diary.presentation.response.DiaryDetailResponse
 import com.example.onui.domain.diary.presentation.response.DiaryListResponse
 import com.example.onui.domain.diary.repository.DiaryRepository
+import com.example.onui.domain.timeline.entity.Timeline
+import com.example.onui.domain.timeline.repository.TimelineRepository
 import com.example.onui.global.common.facade.UserFacade
 import com.example.onui.global.config.error.exception.PermissionDeniedException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.*
 
 @Service
 @Transactional(readOnly = true)
 class DiaryServiceImpl(
     private val diaryRepository: DiaryRepository,
-    private val userFacade: UserFacade
+    private val userFacade: UserFacade,
+    private val timelineRepository: TimelineRepository
 ): DiaryService {
 
     @Transactional
     override fun createDiary(req: CreateDiaryRequest): DiaryDetailResponse {
 
         val user = userFacade.getCurrentUser()
-        val now = LocalDate.now()
+        val now = LocalDateTime.now()
 
         if (diaryRepository.existsByUserAndYearAndMonth(user, now.year, now.monthValue)) throw AlreadyWroteDiaryException
 
         val diary = diaryRepository.save(Diary(
             user,
-            req.text,
+            req.title!!,
+            req.content!!,
             req.mood!!,
+            req.tagList!!,
             now
         ))
 
@@ -53,6 +59,27 @@ class DiaryServiceImpl(
             ?: throw DiaryNotFoundException
 
         if (diary.user != userFacade.getCurrentUser()) throw PermissionDeniedException
+
+        return diary.toDetailResponse()
+    }
+
+    override fun update(req: UpdateDiaryRequest): DiaryDetailResponse {
+        val user = userFacade.getCurrentUser()
+
+        val diary = diaryRepository.findByIdOrNull(req.id)
+            ?: throw DiaryNotFoundException
+
+        if(diary.user != user) throw PermissionDeniedException
+
+        timelineRepository.findByIdOrNull(diary.id)
+            ?.run {
+                timelineRepository.save(Timeline(
+                    diary,
+                    diary.createdAt,
+                    id,
+                    true
+                ))
+            }
 
         return diary.toDetailResponse()
     }
