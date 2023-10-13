7e305abba005dc5b2c3872286a006705572b8bc2
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-13 14:10:59 +0900

    🐛 ::

--- src/main/kotlin/com/example/onui/domain/diary/presentation/DiaryController.kt
+++ src/main/kotlin/com/example/onui/domain/diary/presentation/DiaryController.kt
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -48,7 +49,7 @@ class DiaryController(
         id: UUID
     ): DiaryDetailResponse = diaryService.getDetailById(id)
 
-    @PostMapping
+    @PutMapping
     fun updateDiary(
         @RequestBody @Valid
         req: UpdateDiaryRequest
