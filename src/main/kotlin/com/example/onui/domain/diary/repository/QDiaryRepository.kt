1e38093abe5a3d15113cddea0d3958be2f493df4
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-11-01 21:09:58 +0900

    ⭐️ :: find3DayAgoByUser

---
+++ src/main/kotlin/com/example/onui/domain/diary/repository/QDiaryRepository.kt
@@ -0,0 +1,8 @@
+﻿package com.example.onui.domain.diary.repository
+
+import com.example.onui.domain.diary.entity.Diary
+import com.example.onui.domain.user.entity.User
+
+interface QDiaryRepository {
+    fun find3DayAgoByUser(user: User): Diary?
+}
