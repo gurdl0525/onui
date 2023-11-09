bc6cc3c91b2758eb2cffd18be3d21d420e52b223
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-11-09 11:54:50 +0900

    ⭐️ :: AppleTokenRequest

--- src/main/kotlin/com/example/onui/domain/auth/presentation/dto/request/GoogleCodeRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.onui.domain.auth.presentation.dto.request
-
-data class GoogleCodeRequest(
-    val code: String,
-    val clientId: String,
-    val clientSecret: String,
-    val redirectUri: String,
-    val grantType: String
-)
