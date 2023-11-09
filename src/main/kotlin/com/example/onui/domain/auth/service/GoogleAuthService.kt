803621842d98605d3c401e1c75a70f6155773288
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-11-09 11:47:43 +0900

    ⚡️ :: google oauth 개편

--- src/main/kotlin/com/example/onui/domain/auth/service/GoogleAuthService.kt
+++ src/main/kotlin/com/example/onui/domain/auth/service/GoogleAuthService.kt
@@ -1,11 +1,8 @@
 package com.example.onui.domain.auth.service
 
-import com.example.onui.domain.auth.presentation.dto.response.OauthLinkResponse
 import com.example.onui.domain.auth.presentation.dto.response.TokenResponse
 
 interface GoogleAuthService {
 
-    fun getGoogleLoginLink(): OauthLinkResponse
-
-    fun oauthGoogleSignIn(code: String): TokenResponse
+    fun oauthGoogleSignIn(token: String): TokenResponse
 }
