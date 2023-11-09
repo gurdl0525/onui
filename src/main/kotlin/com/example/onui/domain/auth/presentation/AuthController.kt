033c829584c087c0a359b28adde8d8f462355147
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-11-09 23:21:57 +0900

    📝 :: uri 변경

--- src/main/kotlin/com/example/onui/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/example/onui/domain/auth/presentation/AuthController.kt
@@ -1,13 +1,11 @@
 package com.example.onui.domain.auth.presentation
 
-import com.example.onui.domain.auth.presentation.dto.request.ReissueRequest
 import com.example.onui.domain.auth.presentation.dto.response.TokenResponse
 import com.example.onui.domain.auth.service.AppleAuthService
 import com.example.onui.domain.auth.service.AuthService
 import com.example.onui.domain.auth.service.GoogleAuthService
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-import javax.validation.Valid
 
 @Validated
 @RestController
@@ -18,24 +16,21 @@ class AuthController(
     private val appleAuthService: AppleAuthService
 ) {
 
-    @GetMapping("/oauth/google/token")
+    @GetMapping("/google")
     fun oauthSignIn(
         @RequestParam(name = "token", required = true)
         token: String
     ): TokenResponse = googleAuthService.oauthGoogleSignIn(token)
 
-    @PutMapping("/token")
+    @PutMapping("/re-issue")
     fun reissue(
-        @RequestBody @Valid
-        req: ReissueRequest
-    ): TokenResponse = authService.reissue(req.refreshToken!!)
+        @RequestParam("token", required = true)
+        token: String
+    ): TokenResponse = authService.reissue(token)
 
-    @PostMapping("/oauth/apple/token")
+    @PostMapping("/apple")
     fun oauthSignInWithApple(
-        @RequestParam(name = "code", required = true)
-        code: String
-    ) = appleAuthService.signUp(code)
-
-    @GetMapping("/test")
-    fun test() = appleAuthService.test()
+        @RequestParam(name = "token", required = true)
+        token: String
+    ) = appleAuthService.signUp(token)
 }
