6582a0677d596f0f181bd2765ad52e38479b949e
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-11-09 12:01:35 +0900

    ⭐️ :: Mission Entities

---
+++ src/main/kotlin/com/example/onui/domain/mission/entity/AssignMission.kt
@@ -0,0 +1,24 @@
+﻿package com.example.onui.domain.mission.entity
+
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.DiscriminatorValue
+import javax.persistence.Entity
+
+@Entity
+@DiscriminatorValue("assigned")
+@OnDelete(action = OnDeleteAction.CASCADE)
+class AssignMission(
+    name: String,
+    goal: String,
+    coast: Int,
+    message: String,
+    id: UUID? = null
+): Mission(name, goal, message, id) {
+
+    @Column(name = "coast", nullable = false)
+    var coast: Int = coast
+        protected set
+}
---
+++ src/main/kotlin/com/example/onui/domain/mission/entity/Assigned.kt
@@ -0,0 +1,34 @@
+﻿package com.example.onui.domain.mission.entity
+
+import com.example.onui.domain.user.entity.User
+import org.springframework.data.domain.Persistable
+import java.io.Serializable
+import java.util.UUID
+import javax.persistence.*
+
+@Entity(name = "assigned_table")
+@IdClass(Assigned.IdClass::class)
+class Assigned(
+    user: User,
+    mission: Mission
+): Persistable<Assigned.IdClass> {
+
+    @Id @ManyToOne
+    @JoinColumn(name = "mission_id", nullable = false)
+    var mission: Mission = mission
+        protected set
+
+    @Id @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User = user
+        protected set
+
+    data class IdClass(
+        var mission: UUID? =null,
+        var user: UUID? = null
+    ): Serializable
+
+    override fun getId() = IdClass(this.mission.id, this.user.id)
+
+    override fun isNew() = mission.id == null || user.id == null
+}
