708946d0a1ab7f68d6c9c54558f03209b7cf14b5
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-11-09 23:25:11 +0900

    ⭐️ :: findAssignByCoast

--- src/main/kotlin/com/example/onui/domain/mission/repository/QMissionRepositoryImpl.kt
+++ src/main/kotlin/com/example/onui/domain/mission/repository/QMissionRepositoryImpl.kt
@@ -2,16 +2,19 @@
 
 import com.example.onui.domain.mission.entity.Mission
 import com.example.onui.domain.mission.entity.QMission.mission
+import com.example.onui.domain.mission.entity.QAssignMission.assignMission
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Service
 
 @Service
 class QMissionRepositoryImpl(private val queryFactory: JPAQueryFactory): QMissionRepository {
 
-    fun findByMinusCoast(coast: Int): List<Mission> {
-        return queryFactory.select(mission)
-            .from(mission)
-            .where(mission.coast.eq(coast.plus(1)).or(mission.coast.eq(coast.minus(1))))
-            .fetch()
-    }
+    override fun findAssignByCoast(coast: Int): List<Mission> = queryFactory
+        .select(assignMission)
+        .from(assignMission)
+        .where(
+            assignMission.coast.eq(coast.plus(1))
+                .or(assignMission.coast.eq(coast.minus(1))
+                    .or(assignMission.coast.eq(coast)))
+        ).fetch()
 }
