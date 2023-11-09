bc9a3c151d738287801d250c64bb02735668f962
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-11-09 23:33:15 +0900

    ⭐️ :: redisProperty

---
+++ src/main/kotlin/com/example/onui/global/env/RedisProperty.kt
@@ -0,0 +1,11 @@
+﻿package com.example.onui.global.env
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties("spring.redis")
+data class RedisProperty(
+    val host: String,
+    val port: Int
+)
