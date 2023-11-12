da99ade71f8406b6e6a91064da7d890477385444
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-11-13 08:58:47 +0900

    ⭐️ :: CommentResponse

---
+++ src/main/kotlin/com/example/onui/domain/timeline/presentation/dto/response/CommentResponse.kt
@@ -0,0 +1,10 @@
+﻿package com.example.onui.domain.timeline.presentation.dto.response
+
+import java.util.*
+
+data class CommentResponse(
+    val id: UUID,
+    val timeline: UUID,
+    val user: UUID,
+    val content: String
+)
