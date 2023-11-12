c589a38862698026d3d02566abdd1c5ec5d3f270
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-11-13 08:58:36 +0900

    ⭐️ :: findAllByTimelineOrderByCreatedAtAsc

---
+++ src/main/kotlin/com/example/onui/domain/timeline/repository/CommentRepository.kt
@@ -0,0 +1,13 @@
+﻿package com.example.onui.domain.timeline.repository
+
+import com.example.onui.domain.diary.entity.Diary
+import com.example.onui.domain.timeline.entity.Comment
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface CommentRepository : JpaRepository<Comment, UUID?> {
+
+    fun findAllByTimelineOrderByCreatedAtAsc(timeline: Diary): MutableList<Comment>
+}
