35a6fba3db8313d5e83bd6a0ca0aa916007f79e8
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-11-14 08:46:48 +0900

    ⭐️ :: 유저 Theme

--- src/main/kotlin/com/example/onui/domain/user/presentation/dto/response/UserProfileResponse.kt
+++ src/main/kotlin/com/example/onui/domain/user/presentation/dto/response/UserProfileResponse.kt
@@ -1,8 +1,10 @@
 ﻿package com.example.onui.domain.user.presentation.dto.response
 
-data class UserProfileResponse (
+data class UserProfileResponse(
 
     val sub: String,
 
-    val name: String
+    val name: String,
+
+    val theme: String
 )
